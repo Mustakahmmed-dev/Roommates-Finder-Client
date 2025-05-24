0def29b15b1a5b06483a01e9f11469a3b80b1584
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-05-24 11:19:13 +0600

    Like operations have been implemented

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,7 +1,9 @@
-import { use } from "react";
+import { use, useState } from "react";
 import { useLoaderData, useParams } from "react-router";
 import { AuthContext } from "../contexts/AuthContext";
 import { AiOutlineLike } from "react-icons/ai";
+import Swal from "sweetalert2";
+import { toast } from "react-toastify";
 
 const DetailsPage = () => {
     const { user } = use(AuthContext);
@@ -9,17 +11,47 @@ const DetailsPage = () => {
     const { id } = useParams();
     const loadedData = useLoaderData();
     const singleData = loadedData.find(data => data._id == id);
-
     const { title, rent, location, description, preferences, availability, roomType, name, contact, email } = singleData;
 
+    const [likes, setLikes] = useState(singleData.likeCount || 0);
+    const [disableLike, setDisableLike] = useState(false);
+    const [showContacts, setShowContacts] = useState(false);
+
+
+    const handleLike = async () => {
+        if (user?.email === email) {
+            return Swal.fire({
+                position: "center",
+                icon: "error",
+                title: "You can't like your own post",
+                showConfirmButton: false,
+                timer: 1500
+            });
+        }
+
+        const res = await fetch(`http://localhost:3000/posts/like/${id}`, {
+            method: "PATCH"
+        });
+
+        const data = await res.json();
+
+        if (data.modifiedCount > 0) {
+            toast("Like successful");
+            setLikes(prev => prev + 1);
+            setShowContacts(true);
+            setDisableLike(true);
+        }
+
+    }
     return (
         <div className="w-11/12 md:w-6/12 mx-auto my-14 rounded-lg p-5 bg-cyan-50 space-y-6">
             {/* Poster profile details */}
-            <div className="flex gap-2 justify-center">
+            <div className="flex gap-2 justify-between">
                 <div className="">
                     {/* <img className="rounded-full w-[80px] outline-3 outline-green-500" src={user?.photoURL} alt="" /> */}
                     <h2 className="text-gray-600 font-semibold"> <i>Posted by </i> {name} </h2>
                 </div>
+                <h4> {likes} People interested in </h4>
             </div>
 
             {/* Post details */}
@@ -37,10 +69,11 @@ const DetailsPage = () => {
                     <p>Availability: <span className={`font-semibold ${availability.toLowerCase() === 'available' ? 'text-green-500' : 'text-gray-700'} `}>{availability}</span> </p>
                 </div>
                 <div className="leading-10">
-                    <p>Contact Details: <span className="font-semibold text-gray-800">{contact}</span> </p>
 
-                    <a className="btn bg-cyan-500 text-white" href={`mailto: ${email}`}>Send mail</a>
-                    <button className="btn ml-5 bg-cyan-500 text-white">Like<AiOutlineLike size={20}/> </button>
+                    <p>Contact Details: {showContacts ? <span className="font-semibold text-gray-800">{contact}</span> : <span className="text-gray-400 italic">click on like to see contacts</span>} </p>
+                    {showContacts && <a className="btn bg-cyan-500 text-white" href={`mailto: ${email}`}>Send mail</a>}
+
+                    <button onClick={handleLike} className="btn ml-5 bg-cyan-500 text-white">Like<AiOutlineLike size={20} /> </button>
                 </div>
             </div>
         </div>
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -5,7 +5,7 @@ const ErrorPage = () => {
         <div className="flex flex-col items-center justify-center h-screen gap-2">
             <h1 className="text-3xl font-bold">Oops! Page Not Found</h1>
             <h2 className="text-xl font-semibold">We couldn’t find the page you were looking for.</h2>
-            <p>It might have been removed, renamed, or didn’t exist in the first place.</p>
+            <p>It might have been removed, renamed, or doesn't exist in the website.</p>
             <Link to="/" className="btn btn-neutral">Go back </Link>
         </div>
     )
